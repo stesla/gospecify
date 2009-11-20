33d560e8eddbc27b0332afd67d7e3bd961325c01
Samuel Tesla <stesla@engineyard.com>
2009-11-20 04:49:14 -0600

    Add reporting into new architecture

--- src/specify.go
+++ src/specify.go
@@ -3,10 +3,16 @@ package specify
 import(
 	"fmt";
 	"container/list";
+	"os";
 )
 
 func Run() {
-	runList(behaviors);
+	report := makeReport();
+	runList(behaviors, report);
+	fmt.Println(report.summary());
+	if report.failed > 0 {
+		os.Exit(1);
+	}
 }
 
 func Behavior(name string, description func()) {
@@ -16,15 +22,16 @@ func Behavior(name string, description func()) {
 }
 
 func It(name string, description func()) {
-	currentIt = makeIt(name, description);
+	currentIt = makeIt(name);
+	description();
 	currentBehavior.addIt(currentIt);
 }
 
 type Value interface{}
 
 func That(value Value) (result *that) {
-	result = &that{};
-	result.Should = &should{common{currentBehavior, currentIt, value}};
+	result = makeThat(currentBehavior, currentIt, value);
+	currentIt.addThat(result);
 	return;
 }
 
@@ -51,28 +58,41 @@ func (self *behavior) addIt(it *it) {
 	self.its.PushBack(it);
 }
 
-func (self *behavior) run() {
-	fmt.Println("Running", self.name);
-	runList(self.its);
+func (self *behavior) run(report *report) {
+	runList(self.its, report);
 }
 
 type it struct {
 	name string;
-	description func();
+	thats *list.List;
 }
 
-func makeIt(name string, description func()) (result *it) {
-	result = &it{name, description};
+func makeIt(name string) (result *it) {
+	result = &it{name:name};
+	result.thats = list.New();
 	return;
 }
 
-func (self *it) run() {
-	fmt.Println("  ", self.name);
-	self.description();
+func (self *it) addThat(that *that) {
+	self.thats.PushBack(that);
+}
+
+func (self *it) run(report *report) {
+	runList(self.thats, report);
 }
 
 type that struct {
 	Should matcher;
+}
+
+func makeThat(behavior *behavior, it *it, value Value) *that {
+	common := &common{behavior, it, value};
+	return &that{&should{common:common}};
+}
+
+func (self *that) run(report *report) {
+	runner,_ := self.Should.(runner);
+	runner.run(report);
 }
 
 type matcher interface {
@@ -86,25 +106,36 @@ type common struct {
 }
 
 type should struct {
-	common;
+	*common;
+	block func() (bool, string);
 }
 
 func (self *should) Be(value Value) {
-	if self.common.value != value {
-		fmt.Print("\t");
-		fmt.Printf("expected `%v` to be `%v`", self.common.value, value);
-		fmt.Println("");
+	self.block = func() (bool, string) {
+		if self.common.value != value {
+			error := fmt.Sprintf("%v - %v - expected `%v` to be `%v`", self.common.behavior.name, self.common.it.name, self.common.value, value);
+			return false, error;
+		}
+		return true, "";
+	}
+}
+
+func (self *should) run(report *report) {
+	if pass,msg := self.block(); pass {
+		report.pass();
+	} else {
+		report.fail(msg);
 	}
 }
 
 type runner interface {
-	run();
+	run(*report);
 }
 
-func runList(list *list.List) {
+func runList(list *list.List, report *report) {
 	doList(list, func(item Value) {
 		runner,_ := item.(runner);
-		runner.run();
+		runner.run(report);
 	});
 }
 
@@ -115,4 +146,30 @@ func doList(list *list.List, do func(Value)) {
 		if item == nil { break; }
 		do(item);
 	}
+}
+
+type report struct {
+	passed, failed int;
+	failures *list.List;
+}
+
+func makeReport() *report {
+	return &report{failures:list.New()};
+}
+
+func (self *report) pass() { self.passed++; }
+func (self *report) total() int { return self.passed + self.failed; }
+
+func (self *report) fail(msg string) {
+	self.failures.PushBack(msg);
+	self.failed++;
+}
+
+func (self *report) summary() string {
+	if self.failed > 0 {
+		fmt.Println("\nFAILED TESTS:");
+		doList(self.failures, func(item Value) { fmt.Println("-", item) });
+		fmt.Println("");
+	}
+	return fmt.Sprintf("Passed: %v Failed: %v Total: %v", self.passed, self.failed, self.total());
 }
